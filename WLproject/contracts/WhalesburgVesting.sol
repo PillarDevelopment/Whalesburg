7ef484d90d3edf23e456ba9ad293e1978b9fe9da
PillarDevelopment <2622610@gmail.com>
2018-05-20 20:43:01 +0300

    original

--- WLproject/contracts/WhalesburgVesting.sol
+++ WLproject/contracts/WhalesburgVesting.sol
@@ -6,25 +6,40 @@ import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ER
 import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
+/**
+ * @title TokenVesting
+ * @dev A token holder contract that can release its token balance gradually like a
+ * typical vesting scheme, with a cliff and vesting period. Optionally revocable by the
+ * owner.
+ */
 contract TokenVesting is Ownable {
-
     using SafeMath for uint256;
     using SafeERC20 for ERC20Basic;
 
     event Released(uint256 amount);
     event Revoked();
 
+    // beneficiary of tokens after they are released
     address public beneficiary;
+
     uint256 public cliff;
     uint256 public start;
     uint256 public duration;
     uint256 public tokensAmount;
-
     bool public revocable;
 
     mapping (address => uint256) public released;
     mapping (address => bool) public revoked;
 
+    /**
+     * @dev Creates a vesting contract that vests its balance of any ERC20 token to the
+     * _beneficiary, gradually in a linear fashion until _start + _duration. By then all
+     * of the balance will have vested.
+     * @param _beneficiary address of the beneficiary to whom vested tokens are transferred
+     * @param _cliff duration in seconds of the cliff in which tokens will begin to vest
+     * @param _duration duration in seconds of the period in which the tokens will vest
+     * @param _revocable whether the vesting is revocable or not
+     */
     constructor(
         address _beneficiary,
         uint256 _start,
@@ -46,41 +61,65 @@ contract TokenVesting is Ownable {
         tokensAmount = _tokensAmount;
     }
 
-    /* @notice отправка vested tokens бенефициару*/
+    /**
+     * @notice Transfers vested tokens to beneficiary.
+     * @param token ERC20 token which is being vested
+     */
     function release(ERC20Basic token) public {
         uint256 unreleased = releasableAmount(token);
+
         require(unreleased > 0);
+
         released[token] = released[token].add(unreleased);
+
         token.safeTransfer(beneficiary, unreleased);
+
         emit Released(unreleased);
     }
 
+    /**
+     * @notice Allows the owner to revoke the vesting. Tokens already vested
+     * remain in the contract, the rest are returned to the owner.
+     * @param token ERC20 token which is being vested
+     */
     function revoke(ERC20Basic token) public onlyOwner {
         require(revocable);
         require(!revoked[token]);
+
         uint256 balance = token.balanceOf(this);
+
         uint256 unreleased = releasableAmount(token);
         uint256 refund = balance.sub(unreleased);
+
         revoked[token] = true;
+
         token.safeTransfer(owner, refund);
+
         emit Revoked();
     }
-    /* @dev Вычисляет сумму, которая уже vested но еще не высобожден.*/
+
+    /**
+     * @dev Calculates the amount that has already vested but hasn't been released yet.
+     * @param token ERC20 token which is being vested
+     */
     function releasableAmount(ERC20Basic token) public view returns (uint256) {
         return vestedAmount(token).sub(released[token]);
     }
 
-    /* @dev Вычисляет сумму, которая уже vested.*/
+    /**
+     * @dev Calculates the amount that has already vested.
+     * @param token ERC20 token which is being vested
+     */
     function vestedAmount(ERC20Basic token) public view returns (uint256) {
-        uint256 currentBalance = token.balanceOf(this); // общий баланс токенов на данном счете
-        uint256 totalBalance = currentBalance.add(released[token]); //общий баланс +
+        uint256 currentBalance = token.balanceOf(this);
+        uint256 totalBalance = currentBalance.add(released[token]);
 
         if (block.timestamp < cliff) {
-            return 0; // ничего если еще не прошел холд
+            return 0;
         } else if (block.timestamp >= start.add(duration) || revoked[token]) {
-            return totalBalance; //возврат всех средств если дюрация прошла
+            return totalBalance;
         } else {
-            return totalBalance.mul(block.timestamp.sub(start)).div(duration); // возврт пропорции = общий баланс*(now - star) / продолжительность
+            return totalBalance.mul(block.timestamp.sub(start)).div(duration);
         }
     }
 }
