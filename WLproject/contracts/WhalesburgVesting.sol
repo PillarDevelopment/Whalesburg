74991be5a5eb3cb13d2320abefe710d4ba5c6849
PillarDevelopment <2622610@gmail.com>
2018-05-27 15:26:10 +0300

    Final version for tests: 1) Импортированы исходики Open Zeppelin 2) форматирован стиль

--- WLproject/contracts/WhalesburgVesting.sol
+++ WLproject/contracts/WhalesburgVesting.sol
@@ -1,9 +1,112 @@
 pragma solidity ^0.4.23;
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179 */
+contract ERC20Basic {
+    function totalSupply() public view returns (uint256);
+    function balanceOf(address who) public view returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20 */
+contract ERC20 is ERC20Basic {
+    function allowance(address owner, address spender)
+    public view returns (uint256);
 
-import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20Basic.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/ownership/Ownable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+    function transferFrom(address from, address to, uint256 value)
+    public returns (bool);
+
+    function approve(address spender, uint256 value) public returns (bool);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+/**
+ * @title SafeERC20
+ * @dev Wrappers around ERC20 operations that throw on failure. */
+library SafeERC20 {
+    function safeTransfer(ERC20Basic token, address to, uint256 value) internal {
+        require(token.transfer(to, value));
+    }
+
+    function safeTransferFrom(
+        ERC20 token,
+        address from,
+        address to,
+        uint256 value
+    ) internal {
+        require(token.transferFrom(from, to, value));
+    }
+
+    function safeApprove(ERC20 token, address spender, uint256 value) internal {
+        require(token.approve(spender, value));
+    }
+}
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control */
+contract Ownable {
+    address public owner;
+
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+}
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error */
+library SafeMath {
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        if (a == 0) {
+            return 0;
+        }
+        c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+
+        return a / b;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+/**
+ * @title TokenVesting
+ * @dev A token holder contract that can release its token balance gradually like a
+ * typical vesting scheme, with a cliff and vesting period. Optionally revocable by the owner.*/
 
 contract TokenVesting is Ownable {
     using SafeMath for uint256;
@@ -12,13 +115,12 @@ contract TokenVesting is Ownable {
     event Released(uint256 amount);
     event Revoked();
 
-    // beneficiary of tokens after they are released
     address public beneficiary;
 
     uint256 public cliff;
     uint256 public start;
     uint256 public duration;
-    uint256 public tokensAmount;
+
     bool public revocable;
 
     mapping (address => uint256) public released;
@@ -29,11 +131,9 @@ contract TokenVesting is Ownable {
         uint256 _start,
         uint256 _cliff,
         uint256 _duration,
-        bool _revocable,
-        uint256 _tokensAmount
+        bool _revocable
     )
-    public
-    {
+    public {
         require(_beneficiary != address(0));
         require(_cliff <= _duration);
         beneficiary = _beneficiary;
@@ -41,10 +141,8 @@ contract TokenVesting is Ownable {
         duration = _duration;
         cliff = _start.add(_cliff);
         start = _start;
-        tokensAmount = _tokensAmount;
     }
 
-    /* @notice отправка vested tokens бенефициару*/
     function release(ERC20Basic token) public {
         uint256 unreleased = releasableAmount(token);
 
@@ -57,10 +155,6 @@ contract TokenVesting is Ownable {
         emit Released(unreleased);
     }
 
-    /**
-     * @notice Allows the owner to revoke the vesting. Tokens already vested
-     * remain in the contract, the rest are returned to the owner.
-     */
     function revoke(ERC20Basic token) public onlyOwner {
         require(revocable);
         require(!revoked[token]);
@@ -77,15 +171,14 @@ contract TokenVesting is Ownable {
         emit Revoked();
     }
 
-    /* @dev Вычисляет сумму, которая уже vested но еще не высобожден.*/
     function releasableAmount(ERC20Basic token) public view returns (uint256) {
         return vestedAmount(token).sub(released[token]);
     }
 
-    /* @dev Вычисляет сумму, которая уже vested.*/
     function vestedAmount(ERC20Basic token) public view returns (uint256) {
         uint256 currentBalance = token.balanceOf(this);
         uint256 totalBalance = currentBalance.add(released[token]);
+
         if (block.timestamp < cliff) {
             return 0;
         } else if (block.timestamp >= start.add(duration) || revoked[token]) {
@@ -95,4 +188,3 @@ contract TokenVesting is Ownable {
         }
     }
 }
-// 0xC032D3fCA001b73e8cC3be0B75772329395caA49, 1526461102, 600, 1200, true
