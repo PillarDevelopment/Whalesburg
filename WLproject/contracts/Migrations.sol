e49ea0e40cd13a8e8b2eb3ced17f17a8edc74611
PillarDevelopment <2622610@gmail.com>
2018-06-01 01:20:51 +0300

    update

--- WLproject/contracts/Migrations.sol
+++ WLproject/contracts/Migrations.sol
@@ -1,8 +1,10 @@
 pragma solidity 0.4.24;
 
 contract Migrations {
-  address public owner;
-  uint public last_completed_migration;
+
+ address public owner;
+
+    uint public last_completed_migration;
 
   modifier restricted() {
     if (msg.sender == owner) _;
